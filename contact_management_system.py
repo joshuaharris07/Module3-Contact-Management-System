e6addd9739208202a450b6fdbf1b126c39d7e8d4
joshuaharris07 <joshua.harris07@gmail.com>
2024-09-17 17:46:32 -0600

    Finished code for add_contact() and edit_contact().

--- contact_management_system.py
+++ contact_management_system.py
@@ -1,38 +1,79 @@
 import re
 import os
 
-def add_contact():
-    # while True:
-    name = input("Please enter the first and last name of the new contact: ")
-    phone = input("Please enter their 10 digit phone number (numbers only): ")
-    try:
-        if validate_phone(phone):
-            print("Thank you.")
-        else:
-            print("Please only enter the ten digits of the phone number, no spaces or dashes.")
-    except:
-        print(f"{phone} is not a valid phone.")
-    email = input("Please enter their email address: ")
-    try:
-        if validate_email(email):
-            print("Thank you.")
-        else:
-            print(f"{email} is not valid.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    address = input("Please enter their full address: ")
-    contact_to_add = {"Name": name, "Phone": phone, "Email": email, "Address": address}
-    print(contact_to_add) # will remove before finalizing
-    try: 
-        contact_list[phone] = contact_to_add
-    except:
-        print("It didn't work.")
-    else: 
-        print("It worked!")
-        print(f" New contact list: {contact_list}")
+def add_contact(contact_list):
+    while True:
+        phone_entry = input("Please enter the 10 digit phone number: ")
+        try:
+            if validate_phone(phone_entry):
+                valid, phone = validate_phone(phone_entry)   # Pulls the phone number out of the function for use
+            else:
+                print(f"{phone_entry} is not a valid phone number. Returning to the menu.")
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+        if phone in contact_list:
+            print(f"That phone number is already associated with {contact_list[phone]["name"]} in your contacts. Returning to the menu.")
+            break
+        name = input("Please enter the name of the new contact: ")
+        email = input("Please enter their email address: ")
+        try:
+            if validate_email(email):
+                print("Thank you.")
+            else:
+                print(f"{email} is not a valid email. Returning to the menu.")
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+        address = input("Please enter their full address: ")
+        contact_to_add = {"Name": name, "Phone": phone, "Email": email, "Address": address}
+        print(contact_to_add) # will remove before finalizing
+        try: 
+            contact_list[phone] = contact_to_add
+        except:
+            print("An error occurred adding that contact. Returning to the menu.")
+        else: 
+            print(f"Contact has been successfully added. Returning to the menu.")
+        finally:
+            break
 
-def edit_contact():
-    pass
+def edit_contact(contact_list):
+    while True:
+        phone_entry = input("Please enter the 10 digit phone number: ")
+        try:
+            if validate_phone(phone_entry):
+                valid, phone = validate_phone(phone_entry)   # Pulls the phone number out of the function for use
+            else:
+                print(f"{phone_entry} is not a valid phone number. Returning to the menu.")
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+        if phone not in contact_list:
+                print(f"That phone number is not currently in your contacts. Returning to the menu.")
+                break
+        action = input("What would you like to edit?\n1. Name\n2. Email Address\n 3. Address\n4. Delete the contact\n")
+        if action == "1":
+            name_change = input("Please enter the new name: ")
+            contact_list[phone]["name"] = name_change
+            print(f"The name has successfully been changed to: {name_change}\nReturning to the menu.")
+            break
+        elif action == "2":
+            email_change = input("Please enter the new email address: ")
+            contact_list[phone]["email"] = email_change
+            print(f"The email address has successfully been changed to: {email_change}\nReturning to the menu.")
+            break
+        elif action == "3":
+            address_change = input("Please enter the new address: ")
+            contact_list[phone]["address"] = address_change
+            print(f"The address has successfully been changed to: {address_change}\nReturning to the menu.")
+            break
+        elif action == "4":
+            delete_contact()
+        else:
+            print("That is not a valid selection.")
 
 def delete_contact():
     pass
@@ -72,15 +113,21 @@ def validate_email(email):
     else:
         return False
 
-def validate_phone(phone):
+def validate_phone(phone_entry):
+    phone_list = []
+    for char in phone_entry:
+        if char.isnumeric():
+            phone_list.append(char)
+    phone = "".join(phone_list)
     pattern = r"\d{10}"
     if re.match(pattern, phone):
-        return True
+        return True, phone
     else:
         return False
 
-contact_list = {"9999999999": 
-                {"name": "John Smith", "phone": "9999999999", "email": "email@email.com", "address": "123 main dr. Denver, CO 80221"}
+contact_list = {
+    "9999999999": {"name": "John Smith", "phone": "9999999999", "email": "email@email.com", "address": "123 main dr. Denver, CO 80221"},
+    "7202755650": {"name": "Josh Harris", "phone": "7202755650", "email": "joshua.harris07@gmail.com", "address": "10570 Adams Cir Northglenn, CO 80233"},
 }
 
 print("Welcome to the Contact Managements System!")
@@ -97,9 +144,9 @@ while True:
 """)
 
     if menu_action == "1":
-        add_contact()
+        add_contact(contact_list)
     elif menu_action == "2":
-        edit_contact()
+        edit_contact(contact_list)
     elif menu_action == "3":
         delete_contact()
     elif menu_action == "4":
